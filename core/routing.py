a1d0435c89030a734178ebf43607d00bdc84a2b9
Mykhailo Keda <mriynuk@gmail.com>
2021-02-28 11:08:56 +0000

    Channels 3 migration.

--- core/routing.py
+++ core/routing.py
@@ -1,3 +1,4 @@
+from django.core.asgi import get_asgi_application
 from django.conf.urls import url
 
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -8,10 +9,11 @@ from core.consumers import WsGame
 
 games = ProtocolTypeRouter(
     {
+        "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(
             URLRouter(
                 [
-                    url(r"^ws/game/(?P<game_id>\w+)$", WsGame),
+                    url(r"^ws/game/(?P<game_id>\w+)$", WsGame.as_asgi()),
                 ]
             )
         ),
